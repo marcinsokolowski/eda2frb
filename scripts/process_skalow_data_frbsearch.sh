b0e993f6fc656ca5c27206d5d99010edfe15ab07
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2024-04-27 11:10:52 +0800

    corrections until it works on aavs2-server

--- scripts/process_skalow_data_frbsearch.sh
+++ scripts/process_skalow_data_frbsearch.sh
@@ -4,7 +4,7 @@
 
 dada_files_path=/data/2024_04_22_pulsars/J0835-4510_flagants_90ch_ch230/230
 if [[ -n "$1" && "$1" != "-" ]]; then
-   dada_files_path=="$1"
+   dada_files_path="$1"
 fi
 
 # 1000 -> 1.08us -> ~1080.us ~ 1ms 
@@ -33,6 +33,7 @@ if [[ ! -d ${dada_files_path} ]]; then
 fi
 
 cd ${dada_files_path}
+mkdir -p ${filterbank_dir}
 
 # digifil -t 1000 -o filterbank_1ms/channel_57_1_1713782313.693404.fil channel_57_1_1713782313.693404.dada -b 8
 start_ux=-1
@@ -41,8 +42,9 @@ for dada_file in `ls channel*.dada`
 do
    fil_file=${dada_file%%dada}fil
    base_name=${dada_file%%.dada}
+   start_ux_set=`echo $start_ux | awk '{if($1>0){print 1;}else{print 0;}}'`
    
-   if [[ $start_ux -le 0 ]]; then
+   if [[ $start_ux_set -le 0 ]]; then
       start_ux=`echo ${base_name} | awk '{print substr($1,length($1)-16);}'`
       echo "INFO : start_ux = $start_ux"
    fi
@@ -69,7 +71,12 @@ echo "Total $fil_count filterbank files - FREDDA required divsion by 4 -> proces
 cd ${filterbank_dir}
 
 # merging filterbank files from different coase frequency channels into one BIG file :
-fil_merge_list=`ls *.fil | head --lines=${fil_to_process} | awk '{printf("%s,",$1);}'`
+# fil_merge_list=`ls *.fil | head --lines=${fil_to_process} | awk '{printf("%s,",$1);}'`
+ls channel_?_*.fil > fil_list_all
+ls channel_??_*.fil >> fil_list_all
+ls channel_???_*.fil >> fil_list_all
+fil_merge_list=`cat fil_list_all | head --lines=${fil_to_process} | awk '{printf("%s,",$1);}'`
+
 merged_filfile=merged_${fil_to_process}channels_${start_ux}.fil
 merged_fitsfile=merged_${fil_to_process}channels_${start_ux}.fits
 merged_candfile=merged_${fil_to_process}channels_${start_ux}.cand
