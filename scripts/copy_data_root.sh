2aa4f3c620d751abc87a3d779e21b7c8037321c9
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2024-11-10 08:41:12 +0800

    corrections

--- scripts/copy_data_root.sh
+++ scripts/copy_data_root.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
 dt=`date +%Y_%m_%d_pulsars`
-remote_data_dir="/data2/${dt}/"
 if [[ -n "$1" && "$1" != "-" ]]; then
-   remote_data_dir="$1"  
+  dt="$1"
 fi
 
-local_data_dir="/data/${dt}/"
+remote_data_drive="/data2/"
 if [[ -n "$2" && "$2" != "-" ]]; then
-   local_data_dir="$2"
+   remote_data_drive="$2"  
+fi
+remote_data_dir=${remote_data_drive}/${dt}/
+
+local_data_dir="/data/${dt}/"
+if [[ -n "$3" && "$3" != "-" ]]; then
+   local_data_dir="$3"
 fi
 
 remote_server="msok@eda2-server"
-if [[ -n "$3" && "$3" != "-" ]]; then
-   remote_server="$3"
+if [[ -n "$4" && "$4" != "-" ]]; then
+   remote_server="$4"
 fi
+
+echo "#############################################"
+echo "PARAMETERS:"
+echo "#############################################"
+echo "dt = $dt"
+echo "remote_data_drive = $remote_data_drive"
+echo "remote_data_dir   = $remote_data_dir"
+echo "local_data_dir    = $local_data_dir"
+echo "remote_server     = $remote_server"
+echo "#############################################"
+
 
 echo "-------------------------------- copy_data_root.sh --------------------------------"
 
@@ -27,8 +43,8 @@ if [[ $count_remote -gt 0 && $count_local -le 0 ]]; then
    echo "Starting copying data at:"
    date
    mkdir -p ${local_data_dir}/
-   echo "rsync -avP ${remote_server}:${remote_data_dir}/FRB* ${local_data_dir}/ > ${local_data_dir}/scp.out 2>&1"
-   rsync -avP ${remote_server}:${remote_data_dir}/FRB* ${local_data_dir}/ > ${local_data_dir}/scp.out 2>&1
+   echo "rsync -avP ${remote_server}:${remote_data_dir}/* ${local_data_dir}/ > ${local_data_dir}/scp.out 2>&1"
+   rsync -avP ${remote_server}:${remote_data_dir}/* ${local_data_dir}/ > ${local_data_dir}/scp.out 2>&1
    
    cd ${local_data_dir}/
    date > copied.txt
