f6b94ffe2612dbc9d9275d6722714b84ad3397a1
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2024-12-14 08:26:54 +0800

    added smaller range of DMs

--- scripts/presto_single_pulse_aavs2.sh
+++ scripts/presto_single_pulse_aavs2.sh
@@ -15,12 +15,18 @@ if [[ -n "$3" && "$3" != "-" ]]; then
    numdms=$3
 fi
 
+dmstep=1
+if [[ -n "$4" && "$4" != "-" ]]; then
+   dmstep=$4
+fi
+
 
 echo "##########################################"
 echo "PARAMETERS of presto_single_pulse_aavs2.sh :"
 echo "thresh_sigma = $thresh_sigma"
 echo "filfile      = $filfile"
 echo "numdms       = $numdms"
+echo "dmstep       = $dmstep"
 echo "##########################################"
 
 echo "INFO : activating Python environment with PRESTO single pulse search enabled"
@@ -31,8 +37,8 @@ outdir=presto_sps_thresh${thresh_sigma}
 mkdir -p ${outdir}
 
 # prepsubband :
-echo "prepsubband updated.fil -o ${outdir}/ -numdms $numdms -nsub 640"
-prepsubband updated.fil -o ${outdir}/ -numdms $numdms -nsub 640
+echo "prepsubband updated.fil -o ${outdir}/ -numdms $numdms -nsub 256 -dmstep ${dmstep}"
+prepsubband updated.fil -o ${outdir}/ -numdms $numdms -nsub 256 -dmstep ${dmstep}
 
 # single pulse searches :
 cd ${outdir}/
