e304d16251386df212ead9515a0744705fa88efc
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2024-12-18 16:45:20 +0800

    param added

--- scripts/copy/copyrecentobs.sh
+++ scripts/copy/copyrecentobs.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+n_last=10
+if [[ -n "$1" && "$1" != "-" ]]; then
+   n_last=$1
+fi
+
 cd /data/sdhav2/
 
-/home/msok/bin/lsobs.sh | tail -10 > latestobs.txt
+/home/msok/bin/lsobs.sh | tail --lines=${n_last} > latestobs.txt
 
 for dir in `cat latestobs.txt`
 do
