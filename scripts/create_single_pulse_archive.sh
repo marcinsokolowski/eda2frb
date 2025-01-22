b86862234232c84e155bd247c5db37c8a1c02c23
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2025-01-22 15:27:23 +0800

    corrections in the process

--- scripts/create_single_pulse_archive.sh
+++ scripts/create_single_pulse_archive.sh
@@ -6,18 +6,21 @@ mkdir single_pulse_archives/
 cd single_pulse_archives/
 psrcat -e ${object} > ${object}.eph
 
-
-for dada_file in `ls ../*.dada`
-do
-   echo "ln -s $dada_file"
-   ln -s $dada_file
-done
-
-for dada_file in `ls *.dada`  
+for dada in `ls ../*.dada`  
 do
    outfile=${dada_file%%dada}_single_pulse.ar 
+   dada_file=`basename $dada`
+   
+   dada_dir=${dada_file%%.dada}
+   mkdir -p $dada_dir
+   cd $dada_dir
+   
+   psrcat -e ${object} > ${object}.eph
+   
+   ln -s ../../${dada_file}
 
    # use -cuda 0 when there is GPU     
    echo "dspsr -F1024:D -b1024 -E ${object}.eph -turns 1 -a PSRFITS -minram=256 -B 0.925925926 ${dada_file}"
    dspsr -F1024:D -b1024 -E ${object}.eph -turns 1 -a PSRFITS -minram=256 -B 0.925925926 ${dada_file}
+   cd ..
 done
